fec6211b3fb35a626041036c78cd83e3d92b2855
sakibmohammad79 <sakibmohammad7679@gmail.com>
2024-04-23 21:36:48 +0600

    implement font optimiziation

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import nextImage from "../../assets/istockphoto-539208441-2048x2048.jpg";
 
 const Page = () => {
   return (
@@ -18,6 +19,16 @@ const Page = () => {
         <h2 className="text-center pb-2">Next.Js image optimization</h2>
         <Image
           src="https://thumbs.dreamstime.com/b/all-sports-balls-stadium-d-imaginary-sport-modelled-rendered-78672313.jpg"
+          alt="image"
+          width={500}
+          height={500}
+          className="mx-auto"
+        />
+      </div>
+      <div>
+        <h2 className="text-center pb-2">Local image</h2>
+        <Image
+          src={nextImage}
           alt="image"
           width={500}
           height={500}
